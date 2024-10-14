a3b642cbe967d25892616b42e810c8b056fb9541
Sriram Krishna <sriramsk1999@gmail.com>
2024-10-14 11:49:20 -0400

    real2sim v1

--- robohive/tutorials/ee_teleop.py
+++ robohive/tutorials/ee_teleop.py
@@ -12,19 +12,118 @@ EXAMPLE:\n
 """
 # TODO: (1) Enforce pos/rot/grip limits (b) move gripper to delta commands
 
-from robohive.utils.quat_math import euler2quat, mulQuat
+from robohive.utils.quat_math import euler2quat, mat2quat, mulQuat, quat2mat, euler2mat
 from robohive.utils.inverse_kinematics import IKResult, qpos_from_site_pose
 from robohive.logger.roboset_logger import RoboSet_Trace
 from robohive.logger.grouped_datasets import Trace as RoboHive_Trace
 import numpy as np
 import click
 import gym
+import os
 
 try:
     from vtils.input.keyboard import KeyInput as KeyBoard
     from vtils.input.spacemouse import SpaceMouse
 except ImportError as e:
     raise ImportError("Please install vtils -- https://github.com/vikashplus/vtils")
+import open3d as o3d
+import pickle
+import cv2
+
+def load_hoi4d_trajectory(base_path):
+    """loads a test trajectory from a hardcoded path.
+    The minimum requirement is the hand pose in the
+    camera frame (cam2hand) and the camera pose in the world (world2cam).
+    """
+    cam_trajectory = o3d.io.read_pinhole_camera_trajectory(f"{base_path}/3Dseg/output.log")
+    K = cam_trajectory.parameters[0].intrinsic.intrinsic_matrix
+    cam2camWorld = np.array([i.extrinsic for i in cam_trajectory.parameters])
+    camWorld2cam = np.linalg.inv(cam2camWorld)
+
+    cam2hand = []
+    # in some frames, the hand might not be visible, filter these out
+    idxs = np.array(sorted([int(i.split('.')[0]) for i in os.listdir(f"{base_path}/handpose")]))
+    for idx in idxs:
+        f = open(f"{base_path}/handpose/{idx}.pickle", "rb")
+        data = pickle.load(f)
+        pose = np.eye(4)
+        global_rot = data['poseCoeff'][:3]
+        global_rot = cv2.Rodrigues(global_rot)[0]
+        pose[:3,:3] = global_rot
+        pose[:3, 3] = data['trans']
+        cam2hand.append(pose)
+        f.close()
+    cam2hand = np.array(cam2hand)
+
+    camWorld2hand = camWorld2cam[idxs] @ cam2hand
+    return camWorld2hand
+
+
+def vis_trajectory(traj):
+    """
+    A utility function to visualize trajectory
+    """
+    frames = []
+    trajectory_points = []
+    for pose in traj:
+        frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.01)
+        frame.transform(pose)
+        frames.append(frame)
+        # Extract the translation (position) of each pose for the trajectory line
+        trajectory_points.append(pose[:3, 3])
+
+    # Create a LineSet to visualize the trajectory as a line between points
+    trajectory_points = np.array(trajectory_points)
+    lines = [[i, i + 1] for i in range(len(trajectory_points) - 1)]  # Connect points with lines
+    colors = [[0, 1, 0] for _ in lines]  # Green color for the lines
+
+    # Create the LineSet object
+    trajectory_line = o3d.geometry.LineSet()
+    trajectory_line.points = o3d.utility.Vector3dVector(trajectory_points)
+    trajectory_line.lines = o3d.utility.Vector2iVector(lines)
+    trajectory_line.colors = o3d.utility.Vector3dVector(colors)
+
+    start_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.01)
+    start_sphere.paint_uniform_color([1, 0, 0])  # Red for start
+    start_sphere.translate(trajectory_points[0])
+
+    end_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.01)
+    end_sphere.paint_uniform_color([0, 0, 1])  # Blue for end
+    end_sphere.translate(trajectory_points[-1])
+
+    origin_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
+
+    # Visualize everything
+    o3d.visualization.draw_geometries(frames + [trajectory_line, start_sphere, end_sphere, origin_frame])
+
+def retarget_hand_trajectory(camWorld2hand, robotWorld2ee):
+    """
+    Align hand coordinates with end effector.
+    retarget the 4x4 extrinsics to quaternion/translation/gripper state
+    """
+    scale_factor = 0.6
+    # TODO: Scale translation more intelligently
+    # Scale down the translation of the trajectory
+    # so that end effector does not go out of reach of the robot
+    camWorld2hand[:, :3, 3] *= scale_factor
+
+    robotWorld2camWorld = robotWorld2ee @ np.linalg.inv(camWorld2hand[0])
+    robotWorld2hand = robotWorld2camWorld @ camWorld2hand
+
+    # TODO: Handle rotation properly. Right now its being ignored
+    # Set end effector orientation to identity
+    robotWorld2hand[:, :3, :3] = np.eye(3)
+    # TODO: Validate align_rotation. Is it always necessary?
+    # A rotation to orient the end effector correctly, flipped upside down without this
+    align_rotation = euler2mat((0, np.pi, 0))
+    robotWorld2hand[:, :3, :3] = robotWorld2hand[:, :3, :3] @ align_rotation
+
+    robot_trajectory_quat = mat2quat(robotWorld2hand[:, :3, :3])
+    robot_trajectory_pos = robotWorld2hand[:, :3, 3]
+    # TODO: Handle gripper state properly
+    robot_gripper_state = np.zeros((robotWorld2hand.shape[0], 1))
+    trajectory = np.concatenate([robot_trajectory_pos, robot_trajectory_quat, robot_gripper_state], axis=-1)
+    return trajectory
 
 # Poll and process keyboard values
 def poll_keyboard(input_device):
@@ -35,26 +134,26 @@ def poll_keyboard(input_device):
     done = True if sen=='esc' else False
 
     # gripper
-    if sen == '[':
+    if sen == 'h':
         delta_gripper = -1
-    elif sen == ']':
+    elif sen == 'j':
         delta_gripper = 1
     else:
         delta_gripper = 0
 
     # positions
     delta_pos = np.array([0, 0, 0])
-    if sen == 'd':
+    if sen == 'g':
         delta_pos[0] = 1
-    elif sen == 'a':
+    elif sen == 'f':
         delta_pos[0] = -1
-    elif sen == 'w':
+    elif sen == 'r':
         delta_pos[1] = 1
-    elif sen == 'x':
+    elif sen == 'v':
         delta_pos[1] = -1
-    elif sen == 'q':
+    elif sen == 't':
         delta_pos[2] = 1
-    elif sen == 'z':
+    elif sen == 'b':
         delta_pos[2] = -1
 
     # rotations
@@ -107,7 +206,7 @@ def poll_spacemouse(input_device):
 @click.option('-an', '--action_noise', type=float, default=0.0, help=('Amplitude of action noise during rollout'))
 @click.option('-i', '--input_device', type=click.Choice(['keyboard', 'spacemouse']), help='input to use for teleOp', default='keyboard')
 @click.option('-o', '--output', type=str, default="teleOp_trace.h5", help=('Output name'))
-@click.option('-h', '--horizon', type=int, help='Rollout horizon', default=100)
+@click.option('-h', '--horizon', type=int, help='Rollout horizon', default=1000)
 @click.option('-n', '--num_rollouts', type=int, help='number of repeats for the rollouts', default=1)
 @click.option('-f', '--output_format', type=click.Choice(['RoboHive', 'RoboSet']), help='Data format', default='RoboHive')
 @click.option('-c', '--camera', multiple=True, type=str, default=[], help=('list of camera topics for rendering'))
@@ -151,6 +250,16 @@ def main(env_name, env_args, reset_noise, action_noise, input_device, output, ho
     elif output_format=="RoboSet":
         trace = RoboSet_Trace("TeleOp Trajectories")
 
+    env.reset()
+    curr_pos = env.sim.model.site_pos[goal_sid]
+    curr_quat =  env.sim.model.site_quat[goal_sid]
+    ee_pose = np.eye(4)
+    ee_pose[:3, :3] = quat2mat(curr_quat)
+    ee_pose[:3, 3] = curr_pos
+    base_path = "/home/sriram.sk/desktop/hoi4d_vid/_data_sriram_hoi4d_hoi4d_data_ZY20210800003_H3_C14_N42_S207_s05_T2_vid/"
+    camWorld2hand = load_hoi4d_trajectory(base_path)
+    trajectory = retarget_hand_trajectory(camWorld2hand, ee_pose)
+
     # Collect rollouts
     for i_rollout in range(num_rollouts):
 
@@ -177,16 +286,24 @@ def main(env_name, env_args, reset_noise, action_noise, input_device, output, ho
                 print("Rollout done. ")
                 break
 
-            # recover actions using input ----------------------------
-            # udpate pos
+            # # recover actions using input ----------------------------
+            # # udpate pos
+            # curr_pos = env.sim.model.site_pos[goal_sid]
+            # curr_pos[:] += pos_scale*delta_pos
+            # # update rot
+            # curr_quat =  env.sim.model.site_quat[goal_sid]
+            # curr_quat[:] = mulQuat(euler2quat(rot_scale*delta_euler), curr_quat)
+            # # update gripper
+            # if delta_gripper !=0:
+            #     gripper_state = gripper_scale*delta_gripper # TODO: Update to be delta
+
             curr_pos = env.sim.model.site_pos[goal_sid]
-            curr_pos[:] += pos_scale*delta_pos
+            curr_pos[:] = trajectory[i_step][:3]
             # update rot
             curr_quat =  env.sim.model.site_quat[goal_sid]
-            curr_quat[:] = mulQuat(euler2quat(rot_scale*delta_euler), curr_quat)
+            curr_quat[:] = trajectory[i_step][3:7]
             # update gripper
-            if delta_gripper !=0:
-                gripper_state = gripper_scale*delta_gripper # TODO: Update to be delta
+            gripper_state = trajectory[i_step][7]
 
             # get action using IK
             ik_result = qpos_from_site_pose(
